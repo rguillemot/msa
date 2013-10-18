41980d054f7104c81ec04d3995082bfbdc7317b1
rguillemot <richard_guillemot@yahoo.fr>
2013-10-19 00:12:29 +0200

    wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -28,10 +28,10 @@
 //define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-//define('DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-//define('DB_COLLATE', '');
+define('DB_COLLATE', '');
 
 if (isset($_SERVER["DATABASE_URL"])) {
  $db = parse_url($_SERVER["DATABASE_URL"]);
